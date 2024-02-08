d73e07f5a5b4300907a5fefbe3366034291ceaab
lebrancconvas <lebrancconvas@gmail.com>
2024-02-08 11:29:55 +0700

    :tada: Init.

---
+++ workshop/app/models/user.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  first_name :text
+#  last_name  :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class User < ApplicationRecord
+end
